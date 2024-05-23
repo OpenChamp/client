40b31665aed91d6183a5b16357f8e921ae1376b0
noasakurajin <dev@noasakurajin.de>
2024-05-23 17:37:45 +0200

    actually handle connection errors again

--- Client.gd
+++ Client.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var CountDown = 30
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().paused = true
@@ -21,39 +22,56 @@ func ChangeMap(scene: PackedScene):
 		child.queue_free()
 	map.add_child(scene.instantiate())
 
+# setup the client part of a network peer
 func PrepareClient(peer):
 	print("Preparing Client")
 	
 	peer.create_client("127.0.0.1", 10000)
 	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
 		OS.alert("Failed to start multiplayer client.")
-		return
+		return false
 		
 	print(peer.get_connection_status())
+	return true
 
+# setup the serer part of a peer
 func PrepareServer(peer):
 	print("Preparing Server")
 	peer.create_server(10000, 10)
 	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
 		OS.alert("Failed to Start Server.")
-		return
+		return false
+	
+	return true
 
+# launch a standalone client
 func StartClient():
 	var peer = ENetMultiplayerPeer.new()
-	PrepareClient(peer)
+	if not PrepareClient(peer):
+		return
+	
 	StartGame(peer)
 
+# launch a standalone server
 func StartServer():
 	var peer = ENetMultiplayerPeer.new()
-	PrepareServer(peer)
-	StartGame(peer)
-	
-func StartIntegrated():
-	var peer = ENetMultiplayerPeer.new()
-	PrepareServer(peer)
-	PrepareClient(peer)
+	if not PrepareServer(peer):
+		return
+		
 	StartGame(peer)
 
+# launch a client and a server at the same time
+func StartIntegrated():
+	var peer = ENetMultiplayerPeer.new()
+	if not PrepareServer(peer):
+		return
+		
+	if not PrepareClient(peer):
+		return
+		
+	StartGame(peer)
+
+# actually start the game logic
 func StartGame(peer):
 	print("Starting Game")
 	multiplayer.multiplayer_peer = peer
