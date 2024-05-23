74676076ea18c17560e90d965db289acf1c86742
cmkrist <cmkrist@umich.edu>
2024-05-23 16:14:41 -0400

    Retry Connection

--- Client.gd
+++ Client.gd
@@ -2,9 +2,12 @@ extends Control
 
 @export var CountDown = 30
 
+@export var connection_reset = 3
+@export var max_tries = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().paused = true
+	$ConnectionUI/Reconnect.timeout.connect(TryReconnect)
 	var args = Array(OS.get_cmdline_args())
 	if args.has("-s") || DisplayServer.get_name() == "headless":
 		call_deferred("StartServer")
@@ -26,9 +29,11 @@ func ChangeMap(scene: PackedScene):
 func PrepareClient(peer):
 	print("Preparing Client")
 	
-	peer.create_client("127.0.0.1", 10000)
-	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
-		OS.alert("Failed to start multiplayer client.")
+	var client = peer.create_client("127.0.0.1", 80)
+	if client != MultiplayerPeer.CONNECTION_CONNECTED:
+		$ConnectionUI/RichTextLabel.text = "[center]Failed to connect..."
+		$ConnectionUI/RichTextLabel.fit_content = true
+		RetryClient();
 		return false
 		
 	print(peer.get_connection_status())
@@ -40,7 +45,6 @@ func PrepareServer(peer):
 	peer.create_server(10000, 10)
 	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
 		OS.alert("Failed to Start Server.")
-	
 	return true
 
 # launch a standalone client
@@ -63,19 +67,19 @@ func StartServer():
 func StartIntegrated():
 	var peer = ENetMultiplayerPeer.new()
 	if not PrepareServer(peer):
-		return
-		
-	if not PrepareClient(peer):
-		return
+		PrepareClient(peer)
 		
 	StartGame(peer)
+
+func RetryClient():
+	$ConnectionUI/Reconnect.wait_time = connection_reset
+	$ConnectionUI/Reconnect.start()
 
 # actually start the game logic
 func StartGame(peer):
 	print("Starting Game")
 	multiplayer.multiplayer_peer = peer
 	$ConnectionUI.hide()
-	get_tree().paused = false;
 	if multiplayer.is_server():
 		ChangeMap.call_deferred(load("res://Maps/DebugMap.tscn"))
 		
@@ -85,3 +89,11 @@ func _input(event):
 		if(OS.get_name() != "Windows"):
 			ext = "sh"
 		OS.shell_open(ProjectSettings.globalize_path("res://") + "./run.")
+
+
+func TryReconnect():
+	$ConnectionUI/Reconnect.stop()
+	max_tries -= 1
+	if max_tries <=0:
+		get_tree().quit()
+	StartClient()
