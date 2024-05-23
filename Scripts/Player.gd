5d6171d38d641373784624fcadc1e88c63c87768
cmkrist <cmkrist@umich.edu>
2024-05-23 11:31:30 -0400

    Localize Movement markers

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -26,6 +26,38 @@ func _ready():
 		isPlayer = true;
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
+func _input(event):
+	if event is InputEventMouseButton:
+		# Right click to move
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			Action(event)
+			
+func Action(event):
+	var marker = MoveMarker.instantiate()
+	var from = Camera.project_ray_origin(event.position)
+	var to = from + Camera.project_ray_normal(event.position) * 1000
+	
+	var space = get_world_3d().direct_space_state
+	var params = PhysicsRayQueryParameters3D.create(from, to)
+	var result = space.intersect_ray(params)
+	print_debug(result);
+	# Move
+	if result and result.collider.is_in_group("ground"):
+		result.position.y += 1;
+		marker.position = result.position
+		get_node("/root").add_child(marker);
+		#Player.MoveTo(result.position);
+	# Attack
+	if result and result.collider is CharacterBody3D:
+		print("FOUND YOU")
+		if result.collider.team != Player.team:
+			print("GONNA HURT YOU")
+			#Player.Attack(result.collider)
+
+@rpc("call_local")
+func moveto(coords):
+	pass
+
 func _process(delta):
 	# Get Mouse Coords on screen
 	var mouse_pos = get_viewport().get_mouse_position();
