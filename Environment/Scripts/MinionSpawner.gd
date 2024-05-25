877f8cf319ecf81de84c781de54cb098af5bcfd9
Quokka <jan-niklas@tomski.me>
2024-05-25 11:54:12 +0200

    init minions as class

--- Environment/Scripts/MinionSpawner.gd
+++ Environment/Scripts/MinionSpawner.gd
@@ -3,17 +3,16 @@ extends MultiplayerSpawner
 const MINION_SCENE = preload("res://Characters/Minion.tscn")
 var max_ids:Dictionary
 
-func get_minion(team:int):
+func get_minion(team:int) -> Minion:
 	if not max_ids.has(team):
 		max_ids[team] = 0
 	var id:int = max_ids.get(team)
-	var minion = MINION_SCENE.instantiate()
-	minion.init(id, team)
+	var minion = Minion.new(id, team)
 	max_ids[team] += 1
 	return minion
 	
-func get_wave(team:int, wave_size:int):
-	var minions:Array
+func get_wave(team:int, wave_size:int) -> Array[Minion]:
+	var minions:Array[Minion]
 	for i in wave_size:
 		minions.append(get_minion(team))
 	return minions
--- Scripts/Minion.gd
+++ Scripts/Minion.gd
@@ -1,33 +1,20 @@
-extends CharacterBody3D
+class_name Minion extends CharacterBody3D
 
 @onready var nav_agent = $NavigationAgent3D
+@onready var mesh_instance = $MeshInstance3D
 
-const SPEED = 5.0
-@export var id:int
-@export var team:int
+var speed:float = 5.0
+var id:int
+var team:int
+var range:float = 10.0
+var target_player
 
-func init(init_id:int, init_team:int):
+func _init(init_id:int, init_team:int):
 	id = init_id
 	team = init_team
 	name = "minion_%d_%d" % [team, id]
 	position = get_position()
 
 func _ready():
-	if team == 1:
-		$MeshInstance3D.set_surface_override_material(0, load("res://Environment/Materials/Blue.material"))
-	elif team == 2:
-		$MeshInstance3D.set_surface_override_material(0, load("res://Environment/Materials/Red.material"))
-	# Disable physics process for clients
 	if not multiplayer.is_server():
 		set_physics_process(false)
-
-func _physics_process(delta):
-	var current_location = global_transform.origin
-	var next_location = nav_agent.get_next_path_position()
-	var new_velocity = (next_location - current_location).normalized() * SPEED
-	
-	velocity = new_velocity
-	move_and_slide()
-
-func update_target_location(target_location):
-	nav_agent.target_position = target_location
