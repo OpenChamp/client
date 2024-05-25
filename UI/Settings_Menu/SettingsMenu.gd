0db936aaf8fee0f46a22bdd28cd759ba470a9102
noasakurajin <dev@noasakurajin.de>
2024-05-25 13:53:00 +0200

    get fullscreen toggle working

--- UI/Settings_Menu/SettingsMenu.gd
+++ UI/Settings_Menu/SettingsMenu.gd
@@ -3,10 +3,15 @@ extends Control
 @onready var ConfirmBtn = $SplitContainer/PanelContainer2/GridContainer/ConfirmBtn
 @onready var ExitBtn = $SplitContainer/PanelContainer2/GridContainer/ExitBtn
 
+@onready var fullscreen_toggle = $SplitContainer/PanelContainer/TabContainer/Display/FullscreenToggleBtn
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ExitBtn.pressed.connect(_on_game_close_pressed)
 	ConfirmBtn.pressed.connect(_on_confirm_changes)
+	
+	fullscreen_toggle.button_pressed = Config.is_fullscreen
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,4 +22,5 @@ func _on_game_close_pressed():
 	get_tree().quit()
 
 func _on_confirm_changes():
+	Config.set_fullscreen_mode(fullscreen_toggle.button_pressed)
 	hide()
