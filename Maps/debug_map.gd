e2f66ecd8bbb38e3f17716c4bafd0c09e18fb93e
Quokka <jan-niklas@tomski.me>
2024-05-26 16:19:55 +0200

    adjust minion spawners

--- Maps/debug_map.gd
+++ Maps/debug_map.gd
@@ -4,25 +4,13 @@ extends Node3D
 func _ready():
 	print("Level Loaded")
 
-func spawn_minion_wave(team: int, wave_size: int = 6, delay: float = .5):
-	var wave = $MinionSpawner.get_wave(team, wave_size)
-	for minion in wave:
-		spawn_minion(team, minion)
-		await get_tree().create_timer(delay).timeout
-
-func spawn_minion(team: int = 0, minion = null):
-	if minion != null:
-		$Minions.add_child(minion)
-	else:
-		$Minions.add_child($MinionSpawner.get_minion(team))
-
 ## DEBUG: Spawn minions using a button click #######################################################
 func _input(event):
 	if multiplayer.is_server():
 		# Minion wave for team 1 on "["
 		if event is InputEventKey and event.pressed and event.keycode == KEY_BRACKETLEFT:
-			spawn_minion_wave(1)
+			$MinionSpawner1.spawn_wave()
 		# Single minion for team 2 on "]"
 		if event is InputEventKey and event.pressed and event.keycode == KEY_BRACKETRIGHT:
-			spawn_minion(2)
+			$MinionSpawner2.spawn_minion()
 ####################################################################################################
