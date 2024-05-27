20852fd487fd9fc74f9e9df009b2189c1cfeac94
Jan-Niklas Tomski <jan-niklas@tomski.me>
2024-05-27 14:02:18 +0200

    Update player_stat_interface.gd

--- UI/player_stats/player_stat_interface.gd
+++ UI/player_stats/player_stat_interface.gd
@@ -3,12 +3,3 @@ extends Control
 @onready var healthbar = $ColorRect/BoxContainer/Health
 @onready var manabar = $ColorRect/BoxContainer/Mana
 @onready var expbar = $ColorRect/BoxContainer/Experience
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
