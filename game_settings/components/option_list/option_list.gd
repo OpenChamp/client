7d9f19ca4d59a9afeee6410ee5f26febb688f487
Jan-Niklas Tomski <jan-niklas@tomski.me>
2024-05-27 14:52:13 +0200

    Update option_list.gd

--- game_settings/components/option_list/option_list.gd
+++ game_settings/components/option_list/option_list.gd
@@ -3,7 +3,7 @@ extends ggsUIComponent
 
 @export var use_ids: bool = false
 
-@onready var Btn: OptionButton = $Btn
+@onready var btn: OptionButton = $Btn
 
 
 func _ready() -> void:
@@ -12,28 +12,28 @@ func _ready() -> void:
 		return
 	
 	super()
-	Btn.item_selected.connect(_on_Btn_item_selected)
+	btn.item_selected.connect(_on_btn_item_selected)
 	
-	Btn.pressed.connect(_on_Btn_pressed)
-	Btn.mouse_entered.connect(_on_Btn_mouse_entered)
-	Btn.focus_entered.connect(_on_Btn_focus_entered)
-	Btn.item_focused.connect(_on_Btn_item_focused)
+	btn.pressed.connect(_on_btn_pressed)
+	btn.mouse_entered.connect(_on_btn_mouse_entered)
+	btn.focus_entered.connect(_on_btn_focus_entered)
+	btn.item_focused.connect(_on_btn_item_focused)
 
 
 func init_value() -> void:
 	super()
 	
 	if use_ids:
-		Btn.select(Btn.get_item_index(setting_value))
+		btn.select(btn.get_item_index(setting_value))
 	else:
-		Btn.select(setting_value)
+		btn.select(setting_value)
 
 
-func _on_Btn_item_selected(item_index: int) -> void:
+func _on_btn_item_selected(item_index: int) -> void:
 	GGS.play_sfx(GGS.SFX.INTERACT)
 	
 	if use_ids:
-		setting_value = Btn.get_item_id(item_index)
+		setting_value = btn.get_item_id(item_index)
 	else:
 		setting_value = item_index
 	if apply_on_change:
@@ -44,25 +44,25 @@ func _on_Btn_item_selected(item_index: int) -> void:
 
 func reset_setting() -> void:
 	super()
-	Btn.select(setting_value)
+	btn.select(setting_value)
 
 
 ### SFX
 
-func _on_Btn_pressed() -> void:
+func _on_btn_pressed() -> void:
 	GGS.play_sfx(GGS.SFX.FOCUS)
 
 
-func _on_Btn_mouse_entered() -> void:
+func _on_btn_mouse_entered() -> void:
 	GGS.play_sfx(GGS.SFX.MOUSE_OVER)
 	
 	if grab_focus_on_mouse_over:
-		Btn.grab_focus()
+		btn.grab_focus()
 
 
-func _on_Btn_focus_entered() -> void:
+func _on_btn_focus_entered() -> void:
 	GGS.play_sfx(GGS.SFX.FOCUS)
 
 
-func _on_Btn_item_focused(_index: int) -> void:
+func _on_btn_item_focused(_index: int) -> void:
 	GGS.play_sfx(GGS.SFX.FOCUS)
