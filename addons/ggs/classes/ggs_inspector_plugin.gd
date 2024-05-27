054af6d54428ab7820a99298c196b5aa0a076e13
Jan-Niklas Tomski <jan-niklas@tomski.me>
2024-05-27 14:11:28 +0200

    Update ggs_inspector_plugin.gd

--- addons/ggs/classes/ggs_inspector_plugin.gd
+++ addons/ggs/classes/ggs_inspector_plugin.gd
@@ -2,7 +2,7 @@
 extends EditorInspectorPlugin
 class_name ggsInspectorPlugin
 
-var input_selector_scn: PackedScene = preload("res://addons/ggs/editor/input_selector/input_selector.tscn")
+const InputSelectorScene: PackedScene = preload("res://addons/ggs/editor/input_selector/input_selector.tscn")
 
 
 func _can_handle(object: Object) -> bool:
@@ -13,6 +13,6 @@ func _parse_category(object: Object, category: String) -> void:
 	if category != "input.gd":
 		return
 	
-	var InputSelector: Control = input_selector_scn.instantiate()
-	InputSelector.inspected_obj = object as ggsInputSetting
-	add_custom_control(InputSelector)
+	var input_selector: Control = InputSelectorScene.instantiate()
+	input_selector.inspected_obj = object as ggsInputSetting
+	add_custom_control(input_selector)
