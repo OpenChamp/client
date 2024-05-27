ea9f9f0283845c881232eee650db8eb01cb945b6
Humungus-Fungus <26120733+Humungus-Fungus@users.noreply.github.com>
2024-05-27 11:59:23 -0400

    Working Healthbar and minor recentering optimization

--- UI/player_stats/player_stat_interface.gd
+++ UI/player_stats/player_stat_interface.gd
@@ -3,3 +3,22 @@ extends Control
 @onready var healthbar = $ColorRect/BoxContainer/Health
 @onready var manabar = $ColorRect/BoxContainer/Mana
 @onready var expbar = $ColorRect/BoxContainer/Experience
+
+
+func update_health(hp : int, max_hp : int = -1):
+	healthbar.value = hp
+	if max_hp == -1: return  # Only modify the hp
+	healthbar.max_value = max_hp
+
+
+func update_mana(mp : int, max_mp : int = -1):
+	manabar.value = mp
+	if max_mp == -1: return
+	manabar.max_value = max_mp
+	
+
+
+func update_xp(xp : int, max_xp : int = -1):
+	expbar.value = xp
+	if max_xp == -1: return
+	expbar.max_value = max_xp
