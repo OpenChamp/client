60816e99ac7c53fdedf8b592aa600a3a12f9d11c
Jan-Niklas Tomski <jan-niklas@tomski.me>
2024-05-27 13:33:24 +0200

    Update debug_map.gd

--- Maps/debug_map.gd
+++ Maps/debug_map.gd
@@ -1,32 +1,17 @@
 extends Node3D
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	#$Player.min_x = -30
-	#$Player.max_x = 30
-	#$Player.min_z = -10
-	#$Player.max_z = 10
 	print("Level Loaded")
 
-func spawn_minion_wave(team: int, wave_size: int = 6, delay: float = .5):
-	var wave = $MinionSpawner.get_wave(team, wave_size)
-	for minion in wave:
-		spawn_minion(team, minion)
-		await get_tree().create_timer(delay).timeout
-
-func spawn_minion(team: int = 0, minion = null):
-	if minion != null:
-		$Minions.add_child(minion)
-	else:
-		$Minions.add_child($MinionSpawner.get_minion(team))
 
 ## DEBUG: Spawn minions using a button click #######################################################
 func _input(event):
 	if multiplayer.is_server():
 		# Minion wave for team 1 on "["
 		if event is InputEventKey and event.pressed and event.keycode == KEY_BRACKETLEFT:
-			spawn_minion_wave(1)
+			$MinionSpawner1.spawn_wave()
 		# Single minion for team 2 on "]"
 		if event is InputEventKey and event.pressed and event.keycode == KEY_BRACKETRIGHT:
-			spawn_minion(2)
+			$MinionSpawner2.spawn_minion()
 ####################################################################################################
