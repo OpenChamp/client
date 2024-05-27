b7ef50ee35371b47dc45c134e7a4e9dd939458fc
Jan-Niklas Tomski <jan-niklas@tomski.me>
2024-05-27 14:04:54 +0200

    Update ui.gd

--- UI/ui.gd
+++ UI/ui.gd
@@ -2,12 +2,7 @@ extends Control
 
 @onready var settings_menu = $SettingsMenu
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Handle the player pause action, which opens the settings page
 	if Input.is_action_just_pressed("player_pause"):
@@ -15,4 +10,3 @@ func _process(delta):
 			settings_menu.hide()
 		else:
 			settings_menu.show()
-	
