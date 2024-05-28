ae0a07781821c23f1f07bc491c549a51afe42e76
Quokka <jan-niklas@tomski.me>
2024-05-28 15:30:18 +0200

    Rename minion spawners

--- maps/debug_map.gd
+++ maps/debug_map.gd
@@ -8,10 +8,14 @@ func _ready():
 ## DEBUG: Spawn minions using a button click #######################################################
 func _input(event):
 	if multiplayer.is_server():
-		# Minion wave for team 1 on "["
+		# Minion wave for team 2 on "["
 		if event is InputEventKey and event.pressed and event.keycode == KEY_BRACKETLEFT:
-			$MinionSpawner1.spawn_wave()
-		# Single minion for team 2 on "]"
+			$BlueMinionSpawnerMid.spawn_wave()
+		# Single minion for team 1 on "]"
 		if event is InputEventKey and event.pressed and event.keycode == KEY_BRACKETRIGHT:
+<<<<<<< HEAD:maps/debug_map.gd
 			$MinionSpawner2.spawn_wave()
+=======
+			$RedMinionSpawnerMid.spawn_minion()
+>>>>>>> a59892b (Rename minion spawners):Maps/debug_map.gd
 ####################################################################################################
