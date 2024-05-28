98796d9fa8a609f6c155b84929a16a82715d347e
Cody Krist <137073012+cmkrist@users.noreply.github.com>
2024-05-28 16:03:39 -0400

    Merged

--- UI/ui.gd
+++ UI/ui.gd
@@ -3,8 +3,14 @@ extends Control
 @onready var settings_menu = $SettingsMenu
 @onready var champions = $"../Champions"
 @onready var stats = $CenterContainer/PlayerStatInterface
+@onready var champion: CharacterBody3D = null;
+
+func _ready():
+	get_champion()
 
 func _process(delta):
+	if !champion:
+		get_champion()
 	# Handle the player pause action, which opens the settings page
 	if Input.is_action_just_pressed("player_pause"):
 		if settings_menu.visible:
@@ -13,7 +19,6 @@ func _process(delta):
 			settings_menu.show()
 	
 	check_stats()
-
 
 func check_stats():
 	var pid = multiplayer.get_unique_id()
@@ -24,3 +29,11 @@ func check_stats():
 			# stats.update_xp() I'll leave this for when an xp system is made
 			return
 	stats.update_health(0) # If you can't find the champ they're probably dead
+
+func get_champion():
+	var id = multiplayer.get_unique_id()
+	var champs = $"../Champions".get_children()
+	for champ in champs:
+		if champ.pid == id:
+			champion = champ
+			print("LETS GO!");
