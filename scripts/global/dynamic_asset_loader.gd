d51e2f5e5af506d51838f4af6e748e225e1b0b54
Noa Sakurajin <14131388+noah1510@users.noreply.github.com>
2024-05-31 12:21:13 +0200

    implement patchdata caching (using sha256)

--- scripts/global/dynamic_asset_loader.gd
+++ scripts/global/dynamic_asset_loader.gd
@@ -1,6 +1,8 @@
 extends Node
 
+const HASH_CHUNK_SIZE = 1024
 var asset_map: Dictionary = {}
+
 
 func path_join(parts: Array) -> String:
 	if parts.size() == 0:
@@ -14,10 +16,109 @@ func path_join(parts: Array) -> String:
 	return joined_parts
 
 
+func get_file_hash(path: String) -> String:
+	if not FileAccess.file_exists(path):
+		return ""
+	
+	# Start a SHA-256 context.
+	var ctx = HashingContext.new()
+	ctx.start(HashingContext.HASH_SHA256)
+	
+	var file = FileAccess.open(path, FileAccess.READ)
+	while not file.eof_reached():
+		ctx.update(file.get_buffer(HASH_CHUNK_SIZE))
+	
+	return ctx.finish().hex_encode()
+
+
+func get_string_hash(data: String) -> String:
+	# Start a SHA-256 context.
+	var ctx = HashingContext.new()
+	ctx.start(HashingContext.HASH_SHA256)
+
+	ctx.update(data.to_utf8_buffer())
+
+	return ctx.finish().hex_encode()
+
+
+func has_patch_cached(data_hash: String) -> bool:
+	var patchdata_path = path_join(["user://cache", "patchdata", data_hash + ".json"])
+	return FileAccess.file_exists(patchdata_path)
+
+
+func get_cached_patch(data_hash: String):
+	var patchdata_path = path_join(["user://cache", "patchdata", data_hash + ".json"])
+	if not FileAccess.file_exists(patchdata_path):
+		push_error("No patch with given hash cached: " + patchdata_path)
+		return null
+	
+	var patchdata = ResourceLoader.load(patchdata_path)
+	if not patchdata:
+		push_error("Failed to load patchdata: " + patchdata_path)
+		return null
+	
+	if not patchdata.data is Dictionary:
+		push_error("Invalid patchdata type: " + str(patchdata))
+		return null
+	
+	return patchdata.data as Dictionary
+
+
+func cache_patch_data(data):
+	var data_string: String = ""
+
+	if data is Dictionary:
+		data_string = JSON.stringify(data, "    ")
+	elif data is String:
+		data_string = data
+	else:
+		push_error("Invalid patchdata type: " + str(data))
+		return
+
+	var data_hash = get_string_hash(data_string)
+	if not data_hash:
+		push_error("Failed to hash patchdata")
+		return
+
+	if DynamicAssetLoader.has_patch_cached(data_hash):
+		print("Patchdata already cached: " + data_hash)
+		return
+
+	DirAccess.make_dir_recursive_absolute("user://cache/patchdata")
+
+	var patchdata_path = path_join(["user://cache", "patchdata", data_hash + ".json"])
+	var file = FileAccess.open(patchdata_path, FileAccess.WRITE)
+	if not file:
+		push_error("Failed to open patchdata file for writing: " + patchdata_path)
+		return
+	
+	file.store_string(data_string)
+	file.close()
+
+
 func dump_asset_map():
 	print("runtime assets map:")
 	for key in asset_map.keys():
 		print(str(key) + " -> " + asset_map[key])
+
+
+func load_asset(asset_id: Identifier) -> Resource:
+	if not asset_map.has(asset_id):
+		push_error("Asset not found: " + str(asset_id))
+		return null
+	
+	var asset_path = asset_map[asset_id]
+	var asset = ResourceLoader.load(asset_path)
+	if not asset:
+		push_error("Failed to load asset: " + str(asset_id) + " from " + asset_path)
+		return null
+	
+	return asset
+
+
+func load_texture(asset_id: Identifier) -> Texture:
+	var texture_id = Identifier.new(asset_id.group, "textures/" + asset_id.name)
+	return load_asset(texture_id) as Texture
 
 
 func _ready():
@@ -72,6 +173,8 @@ func _load_asset_group(pack: String, group: String):
 				_load_textures(pack, group)
 			"lang":
 				_load_lang(pack, group)
+			"patchdata":
+				_cache_patchdata(pack, group)
 			"_":
 				print("Unknown asset (" + asset_type + ") in " + pack + "/" + group)
 		
@@ -144,3 +247,32 @@ func _load_lang(pack: String, group: String):
 
 		TranslationServer.add_translation(lang_resource)
 		print("Loaded lang file: " + lang_path)
+
+
+func _cache_patchdata(pack: String, group: String):
+	var patchdata_dir = DirAccess.open(path_join(["user://external", pack, group, "patchdata"]))
+	if not patchdata_dir:
+		print("Failed to open patchdata directory")
+		return
+
+	print("caching patchdata for " + pack + "/" + group)
+
+	patchdata_dir.list_dir_begin()
+	var next_patchdata_name = patchdata_dir.get_next()
+	while next_patchdata_name != "":
+		if patchdata_dir.current_is_dir():
+			push_error("Patchdata files should not be in subdirectories (found in " + pack + "/" + group + "/patchdata)")
+			next_patchdata_name = patchdata_dir.get_next()
+			continue
+
+		var patchdata_name = next_patchdata_name
+		next_patchdata_name = patchdata_dir.get_next()
+
+		if not patchdata_name.ends_with(".json"):
+			push_error("Invalid patchdata file type: " + patchdata_name)
+			continue
+		
+		var patchdata_path = path_join(["user://external", pack, group, "patchdata", patchdata_name])
+		var patch_data_string = FileAccess.open(patchdata_path, FileAccess.READ).get_as_text()
+
+		DynamicAssetLoader.cache_patch_data(patch_data_string)
--- scripts/global/registries.gd
+++ scripts/global/registries.gd
@@ -11,46 +11,76 @@ func get_item_list() -> Array[Item]:
 	return items.values()
 
 
-func serialize_data(json_string):
+func serialize_from_hash(data_hash: String) -> bool:
+	if not DynamicAssetLoader.has_patch_cached(data_hash):
+		print("Patch not found in cache")
+		return false
+
+	var data = DynamicAssetLoader.get_cached_patch(data_hash)
+	if not data:
+		print("Failed to load patch")
+		return false
+
+	if not data is Dictionary:
+		print("Data is not an object")
+		return false
+
+	return _serialize_data(data)
+
+
+func serialize_from_json_string(json_string: String) -> bool:
 	var json_data = JSON.new()
-	var data = json_data.parse(json_string)
+	var err = json_data.parse(json_string)
 
-	if not data.data:
-		push_error("No data found in JSON string")
-		return
+	if err != OK:
+		push_error("Failed to parse JSON data")
+		return false
 
-	if not data.data is Dictionary:
+	if not json_data:
+		push_error("Invalid JSON data")
+		return false
+
+	if not json_data.data is Dictionary:
 		push_error("Data is not an object")
-		return
+		return false
 
-	var data_object = data.data as Dictionary
+	if not _serialize_data(json_data.data):
+		return false
+
+	DynamicAssetLoader.cache_patch_data(json_string)
+	return true
+
+
+func _serialize_data(data_object: Dictionary) -> bool:
 
 	if not data_object.has("format_version"):
 		push_error("No format_version found in JSON data")
-		return
+		return false
 	
 	var format_version = data_object["format_version"].to_int()
 	if format_version != 1:
 		push_error("Invalid format_version found in JSON data %d" % format_version)
-		return
+		return false
 
 	if not data_object.has("item_list"):
 		push_error("No items found in JSON data")
-		return
+		return false
 
 	if not data_object["item_list"] is Array:
 		push_error("Items is not an array")
-		return
+		return false
 
-	_serialize_items_list(data_object["item_list"] as Array)
+	return _serialize_items_list(data_object["item_list"] as Array)
 	
 
-func _serialize_items_list(items_list: Array):
+func _serialize_items_list(items_list: Array) -> bool:
 
 	for item_dict in items_list:
 		if not item_dict is Dictionary:
 			push_error("Item is not an object")
-			return
+			return false
 		
 		var new_item = Item.new(item_dict as Dictionary)
 		items[new_item.get_id()] = new_item
+
+	return true
