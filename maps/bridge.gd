f515490197fffbbff4ba33a972458f97dbae3747
Cody Krist <137073012+cmkrist@users.noreply.github.com>
2024-06-01 11:13:49 -0400

    new waves

--- maps/bridge.gd
+++ maps/bridge.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+@export var countdown = 10
+@export var wave_timeout = 30
+@export var wave_size = 3
 var GameStartTimer = Timer.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,12 +17,12 @@ func _ready():
 func start():
 	GameStartTimer.timeout.disconnect(start);
 	GameStartTimer.queue_free();
-	spawn_waves()
-	$MinionSpawnTimer.wait_time = 10;
+	spawn_waves(wave_size)
+	$MinionSpawnTimer.wait_time = wave_timeout;
 	$MinionSpawnTimer.timeout.connect(spawn_waves)
 	$MinionSpawnTimer.autostart = true;
 	$MinionSpawnTimer.start();
 
-func spawn_waves():
-	$BlueMinionSpawnerMid.spawn_wave()
-	$RedMinionSpawnerMid.spawn_wave()
+func spawn_waves(wave_size = 0):
+	$BlueMinionSpawnerMid.spawn_wave(wave_size)
+	$RedMinionSpawnerMid.spawn_wave(wave_size)
