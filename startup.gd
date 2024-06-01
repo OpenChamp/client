d751a1b8abdd82ebe71d34fe3d45688382a856fd
noasakurajin <dev@noasakurajin.de>
2024-06-01 13:17:55 +0200

    add a launch scene to have the autoload avaiable in startup

--- startup.gd
+++ startup.gd
@@ -182,4 +182,4 @@ func _set_status(message:String):
 
 
 func _update_attempts():
-	attempts_text.text = "[center]" + (tr("STARTUP:ATTEMPTS %s") % attempts) + "[/center]"
+	attempts_text.text = "[center]" + tr("STARTUP:ATTEMPTS") % attempts + "[/center]"
