81a1fbb3459c01ddcc80a506430ddda2a9d820dd
Cody Krist <137073012+cmkrist@users.noreply.github.com>
2024-06-04 18:52:00 -0400

    Load blank map

--- classes/Map.gd
+++ classes/Map.gd
@@ -1,23 +1,28 @@
 extends Node
 class_name Map
 
-@export var connected_players : Array
+@export var connected_players: Array
+
+var Players = {}
 
 func _ready():
 	if not multiplayer.is_server():
 		client_setup()
-		return;
-	pass;
-func _process(_delta):
-	pass;
+		return ;
+	# Spawn all the Champions
+	for player in connected_players:
+		#var champ = load("res://champions/" + player["champ"] + ".tscn").instanciate()
+		#champ.name = player["peer_id"]
+		#champ.nametag = player["name"]
+		#champ.team = player["team"]
+		pass ;
 func _process_delta(_delta):
-	pass;
-
+	pass ;
 
 func client_setup():
 	# Add the player into the world
 	# The player rig will ask the server for their champion
-	var player_rig = load("res://champions/_player.tscn").instanciate()
+	var player_rig = load("res://champions/_player.tscn").instantiate();
 	add_child(player_rig);
 	rpc_id(get_multiplayer_authority(), "client_ready")
 	pass
@@ -26,3 +31,8 @@ func client_setup():
 func client_ready():
 	print(connected_players);
 	print(multiplayer.get_remote_sender_id())
+
+@rpc("any_peer")
+func register_player():
+	var peer_id = multiplayer.get_remote_sender_id()
+	
