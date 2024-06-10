66a36da7015a1793313b2c03278a347c1d7d7390
Noa Sakurajin <14131388+noah1510@users.noreply.github.com>
2024-06-10 18:01:38 +0200

    Cleaned up startup script

--- scenes/startup.gd
+++ scenes/startup.gd
@@ -1,4 +1,5 @@
 extends Node
+
 # This script is explicitly used for setting up the network connection between the client and the server
 
 enum Start {
@@ -7,62 +8,57 @@ enum Start {
 	INTEGRATED,
 }
 
-@onready var args = Array(OS.get_cmdline_args())
+@onready var args := Array(OS.get_cmdline_args())
+
 # Network
-@onready var api_server = "https://api.open-champ.com"
-@onready var address = "127.0.0.1"
-@onready var port = 10000
-@onready var max_players = 2
-@onready var tickrate = 30
-@onready var server_map = "bridge"
+@onready var api_server := "https://api.open-champ.com"
+@onready var address := "127.0.0.1"
+@onready var port := 10000
+@onready var max_players := 2
+@onready var tickrate := 30
+@onready var server_map := "bridge"
 var jwt: String
+
 # UI
-@onready var status_text = $ConnectionUI/Background/ConnectionStatus
-@onready var reconnect_button = $ConnectionUI/Background/ReconnectButton
-@onready var exit_button = $ConnectionUI/Background/ExitButton
-@onready var host_button = $ConnectionUI/Background/HostButton
+@onready var status_text := $ConnectionUI/Background/ConnectionStatus
+@onready var reconnect_button := $ConnectionUI/Background/ReconnectButton
+@onready var exit_button := $ConnectionUI/Background/ExitButton
+@onready var host_button := $ConnectionUI/Background/HostButton
+
 # Server Vars
-var Players: Array = [];
+var Players: Array = []
+
 # Used for when the API fails
-var isTeamOne = true;
+var isTeamOne := true;
 
 func _ready():
-	# UI
 	_set_status("STARTUP:STATUS_CONNECTING")
-	# Parse Args
-	parse_args()
-	# Start Relevant Process
-	if args.has("-s") or DisplayServer.get_name() == "headless":
-		call_deferred("start", Start.SERVER)
-	else:
-		$ConnectionUI.show();
-		# Setup button signals
-		host_button.button_down.connect(host_click)
-		if args.has("-i"):
-			call_deferred("start", Start.INTEGRATED)
-		else:
-			call_deferred("start", Start.CLIENT)
+	
+	# Parse Args and launch the startup deferred
+	var start_type = parse_args()
+	call_deferred("start", start_type)
 
-func start(method: int):
+func start(method: Start):
 	var peer = ENetMultiplayerPeer.new()
-	host_button.hide()
-	reconnect_button.hide()
-	exit_button.hide()
+	
 	# Integrated
 	if method == Start.INTEGRATED:
+		host_button.button_down.connect(host_click)
 		host_button.show()
-		method = Start.CLIENT
-	# Client Side
-	if method == Start.CLIENT:
+	
+	# Init client stuff when not starting dedicated server
+	if method != Start.SERVER:
+		$ConnectionUI.show()
 		setup_client(peer)
-	# Server Side
-	elif method == Start.SERVER:
+	
+	# Start the server
+	if method == Start.SERVER:
 		if not setup_server(peer):
 			server_fail()
-		else:
-			multiplayer.multiplayer_peer = peer
-			server_success()
 			return
+		
+		multiplayer.multiplayer_peer = peer
+		server_success()
 
 # Client Connection Functionality
 func setup_client(peer: ENetMultiplayerPeer):
@@ -70,37 +66,39 @@ func setup_client(peer: ENetMultiplayerPeer):
 	print("Attempting connection to:" + address + ":" + str(port))
 	
 	peer.create_client(address, port)
-	multiplayer.multiplayer_peer = peer;
+	multiplayer.multiplayer_peer = peer
 	_set_status("STARTUP:STATUS_CONNECTING")
+	
 	var timer = Timer.new()
 	timer.name="CheckConnectionTimer"
 	timer.wait_time = 1
-	timer.autostart = true;
+	timer.autostart = true
 	timer.timeout.connect(check_client_connection)
+	
 	add_child(timer)
 
 func check_client_connection():
-	var timer = get_node("CheckConnectionTimer");
-	timer.stop();
-	if multiplayer.multiplayer_peer.get_connection_status() == MultiplayerPeer.CONNECTION_CONNECTING:
-		timer.start()
-		return;
+	var connection_status := multiplayer.multiplayer_peer.get_connection_status()
+	if connection_status == MultiplayerPeer.CONNECTION_CONNECTING:
+		return
+	
+	var timer = get_node("CheckConnectionTimer")
+	timer.stop()
 	timer.timeout.disconnect(check_client_connection)
 	timer.queue_free()
 	
-	if multiplayer.multiplayer_peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
-		client_fail();
-	elif multiplayer.multiplayer_peer.get_connection_status() == MultiplayerPeer.CONNECTION_CONNECTED:
-		client_success();
-	return;
+	if connection_status == MultiplayerPeer.CONNECTION_CONNECTED:
+		client_success()
+	else:
+		client_fail()
 
 func client_success():
-	_set_status("Ready!")
+	_set_status("STARTUP:STATUS_CLIENT_CONNECTED")
 
 func client_fail():
 	_set_status("STARTUP:STATUS_CLIENT_FAILED")
 	multiplayer.multiplayer_peer.close()
-	_set_status("Could not connect to server")
+	
 	reconnect_button.show()
 	exit_button.show()
 
@@ -111,16 +109,20 @@ func setup_server(peer: ENetMultiplayerPeer):
 	# Hook up signals
 	peer.connect("peer_connected", server_add_player)
 	peer.connect("peer_disconnected", server_remove_player)
+	
 	var err = peer.create_server(port, max_players)
 	if err != Error.OK:
 		print("Server failed to start")
 		return false
+	
 	return true
 
 func server_success():
 	print("Server Started, beginning initialization")
+	
 	# Set FPS to 30
-	Engine.set_max_fps(30);
+	Engine.set_max_fps(30)
+	
 	# Set Timer to wait until all players are connected
 	var WaitTimer = Timer.new()
 	WaitTimer.name = "WaitTimer"
@@ -131,82 +133,106 @@ func server_success():
 
 func server_update():
 	var timer = get_node("WaitTimer")
+	
 	# Check how many players are connected
 	var connected_players = multiplayer.get_peers().size()
 	print(str(connected_players) + "/" + str(max_players) + " Connected")
+	
 	if connected_players != max_players:
 		print("Still Waiting...")
-		timer.start();
-	else:
-		print("Ready!")
-		# Clean up our timer
-		timer.stop()
-		timer.timeout.disconnect(server_update)
-		timer.queue_free()
-		# prevent new users from joining
-		multiplayer.multiplayer_peer.refuse_new_connections = true
-		# disconnect the signals
-		multiplayer.multiplayer_peer.disconnect("peer_connected", server_add_player)
-		multiplayer.multiplayer_peer.disconnect("peer_disconnected", server_remove_player)
-		#Change Map
-		print(Players)
-		change_map(load("res://scenes/maps/" + server_map + ".tscn"), Players)
+		timer.start()
+		return
+	
+	print("Ready!")
+	
+	# Clean up our timer
+	timer.stop()
+	timer.timeout.disconnect(server_update)
+	timer.queue_free()
+	
+	# prevent new users from joining
+	multiplayer.multiplayer_peer.refuse_new_connections = true
+	
+	# disconnect the signals
+	multiplayer.multiplayer_peer.disconnect("peer_connected", server_add_player)
+	multiplayer.multiplayer_peer.disconnect("peer_disconnected", server_remove_player)
+	
+	#Change Map
+	print(Players)
+	change_map(load("res://scenes/maps/" + server_map + ".tscn"), Players)
 
 func server_add_player(id: int):
 	print("Player connected: " + str(id))
+	
 	# ask for jwt
 	rpc_id.call_deferred(id, "get_jwt")
 	
 func server_remove_player(id: int):
 	print("Player disconnected: " + str(id))
+	
 	# Remove from Players
 	for i in range(Players.size()):
 		if Players[i].peer_id == id:
 			Players.remove_at(i)
 			break
-	pass ;
 	
 # JWTs
 @rpc("authority", "call_local")
 func get_jwt():
-	if jwt != null:
-		rpc ("set_jwt", jwt)
+	if jwt == null:
+		rpc("set_jwt", "")
 	else:
-		rpc ("set_jwt", "")
+		rpc("set_jwt", jwt)
 
 @rpc("any_peer", "call_local")
 func set_jwt(token: String):
-	var user = {};
+	var user = {}
+	
 	if token == "":
-		var team = 1
-		if !isTeamOne:
-			team = 2
-		isTeamOne = !isTeamOne
-		# Give the user random data
-		var peer_id = multiplayer.get_remote_sender_id()
-		user = {
-			'id': '0', # Local user, no user in DB
-			'peer_id': peer_id,
-			'name': "Player",
-			'champ': "archer",
-			'team': team
-		}
+		# Create a default user in case not token is given
+		user = fetch_default_user()
 	else:
 		# Fetch from the api server
 		user = await fetch_user(token)
-	Players.append(user);
+	
+	Players.append(user)
 
 func fetch_user(token: String):
 	var headers = {}
-	headers["Authorization"] = "Bearer " + token
 	var response = HTTPRequest.new()
-	response.request("GET", api_server + "/user", headers)
+	var message:String = api_server + "/user"
+	
+	headers["Authorization"] = "Bearer " + token
+	response.request("GET", message.split(), headers)
+	
 	while response.get_status() == 0:
 		await response.request_completed
+	
 	if response.get_status() == 200:
 		return response.get_response_body_as_string()
-	else:
-		return null
+	
+	return null
+
+func fetch_default_user():
+	var user = {}
+	
+	var team = 1
+	if not isTeamOne:
+		team = 2
+	
+	isTeamOne = !isTeamOne
+	
+	# Give the user random data
+	var peer_id = multiplayer.get_remote_sender_id()
+	user = {
+		'id': '0', # Local user, no user in DB
+		'peer_id': peer_id,
+		'name': "Player",
+		'champ': "archer",
+		'team': team
+	}
+	
+	return user
 
 func server_fail():
 	OS.alert("Server failed to start")
@@ -215,39 +241,49 @@ func server_fail():
 # Custom Functions
 func change_map(scene: PackedScene, players):
 	var map = $Map
+	
 	# Clean out everything
 	for child in map.get_children():
 		map.remove_child(child)
 		child.queue_free()
+	
 	var new_map = scene.instantiate()
 	new_map.add_to_group("Map")
 	new_map.connected_players = players
 	map.add_child(new_map)
 	rpc("map_loaded")
 
-func parse_args():
+func parse_args() -> Start:
+	var start_type = Start.CLIENT
+	if DisplayServer.get_name() == "headless":
+		start_type = Start.SERVER
+	
 	for i in args.size():
-		if args[i].begins_with("-"):
-			# Process as command
-			# Server
-			if args[i] == "-S":
+		match args[i]:
+			"-S": # Server
 				address = args[i + 1]
-			# Port
-			if args[i] == "-P":
+				i += 1
+			"-P": # Port
 				port = args[i + 1]
-			# Map
-			if args[i] == "-M":
+				i += 1
+			"-M": # Map
 				server_map = args[i + 1]
-			# Tickrate
-			if args[i] == "-T":
+				i += 1
+			"-T": # Tickrate
 				Engine.physics_ticks_per_second = args[i + 1]
-			# Players (Max)
-			if args[i] == "-PL":
+				i += 1
+			"-PL": # Players (Max)
 				max_players = args[i + 1]
-				pass
-			# token
-			if args[i] == "-t":
+				i += 1
+			"-t": # token
 				jwt = args[i + 1]
+				i += 1
+			"-s": # start as server
+				start_type = Start.SERVER
+			"-i": # start as integrated server
+				start_type = Start.INTEGRATED
+	
+	return start_type
 
 # Event Listeners
 func host_click():
