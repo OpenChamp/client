b3c3f496726fd5162a5947a7d7529edc788102eb
InAJacket <InAJacket@fastmail.com>
2024-06-18 00:58:53 +1000

    Champion logic consolidation
    All champion scenes should now use the `champion.gd` script, and inherit from
    `champion.tscn`.
    
    There is currently some movement stutter which I'm working to address
    before opening a PR.

--- classes/champion.gd
+++ classes/champion.gd
@@ -10,6 +10,18 @@ class_name Champion
 @export var mana_regen: float = 5
 
 
+func _ready():
+	super()
+	healthbar.size = Vector2(100, 15)
+	global_position = server_position
+	
+
+func die():
+	super()
+	var server_listener = $"../../ServerListener"
+	server_listener.rpc_id(multiplayer.get_unique_id(), "respawn", self)
+
+
 @rpc("authority", "call_local")
 func change_state(new, args):
 	$StateMachine.change_state(new, args);
