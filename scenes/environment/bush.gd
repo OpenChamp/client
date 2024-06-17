499a05defc87de1112d5737aba11f35dc93fd06f
Cody Krist <137073012+cmkrist@users.noreply.github.com>
2024-06-17 18:55:00 -0400

    Healthbar hides on hidden

--- scenes/environment/bush.gd
+++ scenes/environment/bush.gd
@@ -2,10 +2,8 @@ extends Area3D
 
 func _on_body_entered(body):
 	body.hide()
-	body.get_node("Healthbar").hide()
 	pass # Replace with function body.
 
 func _on_body_exited(body):
 	body.show()
-	body.get_node("Healthbar").hide()
 	pass # Replace with function body.
