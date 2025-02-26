a57dd87bd9b6595cf22d33b46dd5420dcfe0f134
Cody Krist <cmkrist@umich.edu>
2025-02-26 16:12:07 -0500

    Champ Select init

---
+++ ui/champ_select.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _ready() -> void:
+    pass
+
+func _on_ranger_champ_button_up() -> void:
+    # Tell the server you want to play as a ranger
+    
+	pass # Replace with function body.
