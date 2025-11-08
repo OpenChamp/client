9b04f0dd062b0128ee8467882820e1da2be2c003
cmkrist <cmkrist@umich.edu>
2025-11-07 23:20:28 -0500

    ping timer fixes

--- scripts/utility/ping_timer.gd
+++ scripts/utility/ping_timer.gd
@@ -4,7 +4,7 @@ signal ping_response(id: int, latency: int)
 var PING_START:int = 0
 
 func _ready() -> void:
-	if not ConfigManager.CLIENTMODE.DEDICATED_SERVER:	 pass;
+	if ConfigManager.client_mode != ConfigManager.CLIENTMODE.DEDICATED_SERVER: return;
 	timeout.connect(_on_ping_timer_timeout)
 	start()
 
@@ -12,13 +12,13 @@ func _ready() -> void:
 func ping():
 	rpc_id(1, "pong")
 
-func _on_ping_timer_timeout() -> void:
-	if not ConfigManager.CLIENTMODE.DEDICATED_SERVER: stop(); return
-	PING_START = Time.get_ticks_msec()
-	rpc("ping")
-
 @rpc("any_peer")
 func pong():
 	var now = Time.get_ticks_msec()
 	var id = multiplayer.get_remote_sender_id()
 	emit_signal("ping_response", id, now - PING_START)
+
+func _on_ping_timer_timeout() -> void:
+	if not ConfigManager.CLIENTMODE.DEDICATED_SERVER: stop(); return
+	PING_START = Time.get_ticks_msec()
+	rpc("ping")
