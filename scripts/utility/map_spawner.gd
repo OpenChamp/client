818f88f19129362a0bcbfb92119f0acb2a2c8f83
cmkrist <cmkrist@umich.edu>
2025-11-24 15:43:17 -0500

    remove dead utility code

--- scripts/utility/map_spawner.gd
+++
@@ -1,9 +0,0 @@
-extends MultiplayerSpawner
-
-func spawn_map(map_name:String):
-	if !multiplayer.is_server(): return
-	print("Attempting to spawn map")
-	var map_scene = load(str("res://scenes/maps/", map_name, ".tscn"))
-	var map = map_scene.instantiate()
-	map.name="GameMap"
-	get_node(spawn_path).call_deferred("add_child", map, true)
