6610363f59eddf8659a68d3c1530cd4753651c63
cmkrist <cmkrist@umich.edu>
2025-12-02 13:48:46 -0500

    remove broken asset_placer plugin

--- addons/asset_placer/data/asset_folder.gd
+++
@@ -1,9 +0,0 @@
-extends RefCounted
-class_name AssetFolder
-
-var path: String
-var include_subfolders: bool
-
-func _init(path: String, include_subfolders: bool = false):
-	self.path = path
-	self.include_subfolders = include_subfolders
--- addons/asset_placer/updater/plugin_config.gd
+++
@@ -1,9 +0,0 @@
-extends RefCounted
-class_name PluginConfiguration
-
-var version: Version
-
-func _init(file_path: String):
-	var config = ConfigFile.new()
-	config.load(file_path)
-	self.version = Version.new(config.get_value("plugin", "version", "unknown"))
